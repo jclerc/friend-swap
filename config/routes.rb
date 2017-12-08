294245f90c483bfb9330784461f14b9a4bccb632
JClerc <jices25@gmail.com>
2017-12-08 18:19:17 +0100

    Add pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/index'
+
+  root 'pages#index'
 
   devise_for :users, path: 'auth', path_names: {
     sign_up: 'register',
