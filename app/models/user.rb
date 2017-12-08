94eed4c7341ebeaa3e479a63248d2d53d0e266f1
JClerc <jices25@gmail.com>
2017-12-08 18:52:00 +0100

    Add firstname and phone to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence: true, length: { maximum: 30 }
+  validates :phone, presence: true, length: { minimum: 10, maximum: 10 }
 end
