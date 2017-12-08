e57e86e1b5869f9c2a62bb80ad63bc834b633a6f
JClerc <jices25@gmail.com>
2017-12-08 20:44:26 +0100

    Add is_admin to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171208173849) do
+ActiveRecord::Schema.define(version: 20171208194347) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171208173849) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "phone"
+    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
