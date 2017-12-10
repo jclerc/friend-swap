70818b6be04cbdf1b5ba9f11f30ba2b2dac00ea0
JClerc <jices25@gmail.com>
2017-12-10 16:10:11 +0100

    Add seeds to fill database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create users
+user_alex = User.create! email: 'alex@test', password: 'test@test', password_confirmation: 'test@test', first_name: 'Alex', phone: '0611223344'
+user_zoe = User.create! email: 'zoe@test', password: 'test@test', password_confirmation: 'test@test', first_name: 'Zoé', phone: '0699887766'
+
+# Cities
+city_paris = City.create! name: 'Paris', latitude: 48.864716, longitude: 2.349014
+
+# Tags
+tag_sport = Tag.create! label: 'Sportif'
+tag_creative = Tag.create! label: 'Créatif'
+tag_cook = Tag.create! label: 'Cuisine'
+tag_travel = Tag.create! label: 'Voyage'
+
+# Friends
+friend_bastien = Friend.create!(
+  first_name: 'Bastien',
+  birthday: Date.new(1980, 1, 1),
+  is_male: true,
+  description: 'Son nom est Bastien',
+  city: city_paris,
+  user: user_alex
+)
+
+friend_yvan = Friend.create!(
+  first_name: 'Yvan',
+  birthday: Date.new(1981, 12, 30),
+  is_male: true,
+  description: 'Voici Yvan',
+  city: city_paris,
+  user: user_zoe
+)
+
+# First tags
+TagRelation.create! exchange: nil, tag: tag_travel, friend: friend_bastien
+TagRelation.create! exchange: nil, tag: tag_sport, friend: friend_bastien
+TagRelation.create! exchange: nil, tag: tag_creative, friend: friend_yvan
+
+# Old exchange
+exchange_old = Exchange.create! is_active: false, friend1: friend_yvan, friend2: friend_bastien
+TagRelation.create! exchange: exchange_old, tag: tag_sport, friend: friend_bastien
+TagRelation.create! exchange: exchange_old, tag: tag_creative, friend: friend_yvan
+TagRelation.create! exchange: exchange_old, tag: tag_cook, friend: friend_yvan
