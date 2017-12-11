00c60ef9334735598c892223fb8409a51d81869f
JClerc <jices25@gmail.com>
2017-12-11 17:36:09 +0100

    Rewrite tags by city query

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,42 @@
 class PagesController < ApplicationController
   def index
     @city = City.first
-    @tags_by_city = Tag.select('tags.*, cities.name as city_name, cities.id as city_id, COUNT(DISTINCT friends.id) as count')
-                       .joins(:tag_relations)
-                       .joins(:friends)
-                       .joins('INNER JOIN cities ON cities.id = friends.city_id')
-                       .group('friends.city_id')
-                       .order('count DESC')
     @most_traded = Friend.select('friends.*, COUNT(DISTINCT tag_relations.exchange_id) as exchanges_count')
                          .joins(:tag_relations)
                          .group(:id)
                          .order('exchanges_count DESC')
+
+    # NOTE: here we are quering then filtering results by ourselves...
+    tags_by_city = Tag.select('tags.*, '\
+                              'cities.id as city_id, '\
+                              'cities.name as city_name, '\
+                              'COUNT(DISTINCT friends.id) as count')
+                      .joins(:tag_relations)
+                      .joins(:friends)
+                      .joins('INNER JOIN cities ON cities.id = friends.city_id')
+                      .group('cities.id, tags.id')
+    hash = Hash.new(0)
+    tags_by_city.each do |tag|
+      hash[tag.city_id] = [tag.count, tag] if tag.count > hash[tag.city_id][0]
+    end
+    @tags_by_city = hash.values.map { |entry| entry[1] }
+
+    # ... as the following query works in SQLite, but not in PostgreSQL
+    # @tags_by_city = Tag.select('id, label_male, city_name, city_id, MAX(friends_count) as count')
+    #                    .from(
+    #                      Tag.select(
+    #                        'tags.*, '\
+    #                        'cities.id as city_id, '\
+    #                        'cities.name as city_name, '\
+    #                        'COUNT(DISTINCT friends.id) as friends_count'
+    #                      )
+    #                      .joins(:tag_relations)
+    #                      .joins(:friends)
+    #                      .joins('INNER JOIN cities ON cities.id = friends.city_id')
+    #                      .group('cities.id, tags.id')
+    #                   )
+    #                   .group('city_id')
+    #                   .order('count DESC')
   end
 
   def search
