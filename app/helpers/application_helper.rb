db52b693aba7be237434edf7452b419a0b2c39ef
JClerc <jices25@gmail.com>
2017-12-13 14:03:04 +0100

    Display rails notices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,19 @@ module ApplicationHelper
       link_to link_text, link_path, class: 'nav-link'
     end
   end
+
+  def bootstrap_class_for flash_type
+    case flash_type.to_sym
+      when :success
+        "alert-success"
+      when :error
+        "alert-error"
+      when :alert
+        "alert-block"
+      when :notice
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
 end
