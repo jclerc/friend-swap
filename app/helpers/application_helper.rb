3302aacdb0d3eefbb342529e6968d6d0e0b21f46
JClerc <jices25@gmail.com>
2017-12-13 13:22:38 +0100

    Add navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def nav_link(link_text, link_path)
+    class_name = "nav-item #{current_page?(link_path) ? 'active ' : ''}"
+
+    content_tag(:li, class: class_name) do
+      link_to link_text, link_path, class: 'nav-link'
+    end
+  end
 end
