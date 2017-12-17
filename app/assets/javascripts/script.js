fe1f9bf7a822b52c11b6593a8178f9de0b441d2b
JClerc <jices25@gmail.com>
2017-12-17 23:52:46 +0100

    Fix some navbar stuff

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,15 +1,19 @@
-$(function() {
+$(function () {
   var items = $('.search-city, .search-tag');
   var counter = 0;
   var elementSaver;
-  var normalize = function(text) {
+  var normalize = function (text) {
     return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/ /g, '').toLowerCase()
-  }
+  };
+  var reset = function () {
+    $('.search-form').val('').focus();
+    items.show();
+  };
 
-  $('.search-form').on('keyup', function() {
+  $('.search-form').on('keyup', function () {
     var search = normalize($(this).val());
     counter = 0;
-    items.each(function() {
+    items.each(function () {
       var $this = $(this);
       var text = normalize($this.find('.custom-control-description').text());
       if (text.search(search) > -1) {
@@ -18,20 +22,19 @@ $(function() {
         elementSaver = $this;
       } else {
         $this.hide();
-      };
+      }
     });
   });
 
-  $('.search-form').on('keydown', function(element) {
-    if (element.keyCode == 13 && counter == 1) {
-      elementSaver.find('input').prop('checked', true);
-      $('.search-form').val('').focus();
-      items.show();
-    };
+  $('.search-form').on('keydown', function (e) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      if (counter === 1) {
+        elementSaver.find('input').prop('checked', true);
+        reset();
+      }
+    }
   });
 
-  $('.search-reset').on('click', function() {
-    $('.search-form').val('').focus();
-    items.show();
-  });
+  $('.search-reset').on('click', reset);
 });
